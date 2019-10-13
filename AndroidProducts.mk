047b2a6836d1034614fb917714d9372017ce14a6
Mountaser Halak <mountaser1998@gmail.com>
2019-10-13 06:02:53 +0530

    davinci: Havoc-ify

---
+++ AndroidProducts.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/havoc_davinci.mk
+
+COMMON_LUNCH_CHOICES := \
+    havoc_davinci-userdebug \
+    havoc_davinci-eng
---
+++ havoc_davinci.mk
@@ -0,0 +1,22 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from davinci device
+$(call inherit-product, device/xiaomi/davinci/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/havoc/config/common.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := havoc_davinci
+PRODUCT_DEVICE := davinci
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
+    ro.product.model
